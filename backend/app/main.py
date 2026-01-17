b62023deadb43de58f522e3d7f03c12ef206bed2
rubenyh <rubenyanezhernandez@gmail.com>
2026-01-16 19:51:15 -0800

    initial commit

---
+++ backend/app/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI(title="Fusion")
+
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+def root():
+    return {"message": "FastAPI backend running"}
